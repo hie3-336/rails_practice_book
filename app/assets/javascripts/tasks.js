8aed0f320172b93b78586d21ff7394a25501d34a
hie3-336 <tomohiko336@gmail.com>
2024-07-21 15:35:58 +0900

    タスクの背景色をカーソル時変更するJS追加 8-1

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,14 @@
+document.addEventListener('turbolinks:load', function() {
+  document.querySelectorAll('td').forEach(function(td) {
+    td.addEventListener('mouseover', function(e) {
+      // const msg = 'Hello';
+
+      // console.log(msg);
+      e.currentTarget.style.backgroundColor = '#eff';
+    });
+
+    td.addEventListener('mouseout', function(e) {
+      e.currentTarget.style.backgroundColor = '';
+    });
+  });
+});
